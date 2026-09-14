2b87d27e68e470bc24fc65a032a548b22254f118
Muhammed Anshid <anshianshid58@gmail.com>
2026-09-14 12:41:10 +0530

    Fix: remove SSL OPTIONS from DATABASES (match car wash pattern)

--- parishudam_project/settings.py
+++ parishudam_project/settings.py
@@ -93,9 +93,6 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
     }
 }
 
